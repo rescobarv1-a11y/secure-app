61852291ccee95422324d2d27264bd3a509bbeba
Rebeca Escobar <rescobarv1@miumg.edu.gt>
2025-10-16 20:01:23 -0600

    Primer commit secure-app Rebeca

---
+++ src/app.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('secure-app corriendo');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server en puerto ${PORT}`));
